232febc417c449d10c4ffad7fce3feedba989406
mcxtzhang <mcxtzhang@163.com>
2017-02-17 16:17:45 +0800

    【第七章 】private 的方法就是final的

---
+++ src/com/mcxtzhang/chapter7/BaseFinal.java
@@ -0,0 +1,16 @@
+package com.mcxtzhang.chapter7;
+
+/**
+ * Intro:private 的方法就是final的
+ * Author: zhangxutong
+ * E-mail: mcxtzhang@163.com
+ * Home Page: http://blog.csdn.net/zxt0601
+ * Created:   2017/2/17.
+ * History:
+ */
+public class BaseFinal {
+
+    private  void method(){
+        System.out.println("method");
+    }
+}
---
+++ src/com/mcxtzhang/chapter7/ChildFinal.java
@@ -0,0 +1,17 @@
+package com.mcxtzhang.chapter7;
+
+/**
+ * Intro:private 的方法就是final的
+ * Author: zhangxutong
+ * E-mail: mcxtzhang@163.com
+ * Home Page: http://blog.csdn.net/zxt0601
+ * Created:   2017/2/17.
+ * History:
+ */
+public class ChildFinal extends BaseFinal {
+
+    //@Override
+    public final void method(){
+        System.out.println("method");
+    }
+}
