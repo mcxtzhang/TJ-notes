8efcadb01574656eb09d0cbc1efc706e91588200
mcxtzhang <mcxtzhang@163.com>
2017-02-17 15:28:17 +0800

    【第七章 】空白final 一个空白fianl类(final修饰数据，必须在定义时 或者 构造器里初始化)

---
+++ src/com/mcxtzhang/chapter7/BlankFinal.java
@@ -0,0 +1,17 @@
+package com.mcxtzhang.chapter7;
+
+/**
+ * Intro:  一个空白fianl类(final修饰数据，必须在定义时 或者 构造器里初始化)
+ * Author: zhangxutong
+ * E-mail: mcxtzhang@163.com
+ * Home Page: http://blog.csdn.net/zxt0601
+ * Created:   2017/2/17.
+ * History:
+ */
+public class BlankFinal {
+    private final int i;
+
+    public BlankFinal() {
+        i = 1;
+    }
+}
---
+++ src/com/mcxtzhang/chapter7/BlankFinalTest.java
@@ -0,0 +1,15 @@
+package com.mcxtzhang.chapter7;
+
+/**
+ * Intro: 空白Final 测试
+ * Author: zhangxutong
+ * E-mail: mcxtzhang@163.com
+ * Home Page: http://blog.csdn.net/zxt0601
+ * Created:   2017/2/17.
+ * History:
+ */
+public class BlankFinalTest {
+    public static void main(String[] args) {
+
+    }
+}
