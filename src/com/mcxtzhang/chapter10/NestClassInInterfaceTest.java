d981f9211cf9cc7bb81fe5b1eaeff5f070e8205a
mcxtzhang <mcxtzhang@163.com>
2017-03-23 15:41:38 +0800

    【Chap 10】接口内部的类自动是 public static

---
+++ src/com/mcxtzhang/chapter10/NestClassInInterfaceTest.java
@@ -0,0 +1,26 @@
+package com.mcxtzhang.chapter10;
+
+/**
+ * Intro: 接口内部的类自动是 public static
+ * Author: zhangxutong
+ * E-mail: mcxtzhang@163.com
+ * Home Page: http://blog.csdn.net/zxt0601
+ * Created:   2017/3/23.
+ * History:
+ */
+public interface NestClassInInterfaceTest {
+    void method();
+
+    class Impl implements NestClassInInterfaceTest {
+
+        @Override
+        public void method() {
+            System.out.println("hahahah");
+        }
+
+        public static void main(String[] args) {
+            NestClassInInterfaceTest nestClassInInterfaceTest = new Impl();
+            nestClassInInterfaceTest.method();
+        }
+    }
+}
