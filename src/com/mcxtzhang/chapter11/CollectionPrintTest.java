3671fff9b77b4150896cea7fe4ee48fa44d53939
mcxtzhang <mcxtzhang@163.com>
2017-03-24 17:12:11 +0800

    【Chap 11】集合输出测试，集合输出形式为 [] 和 {}形式

---
+++ src/com/mcxtzhang/chapter11/CollectionPrintTest.java
@@ -0,0 +1,19 @@
+package com.mcxtzhang.chapter11;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Intro: 集合输出测试，集合输出形式为 [] 和 {}形式
+ * Author: zhangxutong
+ * E-mail: mcxtzhang@163.com
+ * Home Page: http://blog.csdn.net/zxt0601
+ * Created:   2017/3/24.
+ * History:
+ */
+public class CollectionPrintTest {
+    public static void main(String[] args) {
+
+        System.out.println(new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4)));
+    }
+}
---
+++ src/com/mcxtzhang/chapter11/IteratorRemoveTest.java
@@ -0,0 +1,32 @@
+package com.mcxtzhang.chapter11;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Intro: 迭代器 Iterator remove 影不影响srcList
+ * Author: zhangxutong
+ * E-mail: mcxtzhang@163.com
+ * Home Page: http://blog.csdn.net/zxt0601
+ * Created:   2017/3/24.
+ * History:
+ */
+public class IteratorRemoveTest {
+    public static void main(String[] args) {
+        List src = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            src.add(i);
+        }
+        System.out.println(src);
+
+        Iterator iterator = src.iterator();
+        while (iterator.hasNext()) {
+            iterator.next();
+            iterator.remove();
+        }
+
+        System.out.println(src);
+
+    }
+}
