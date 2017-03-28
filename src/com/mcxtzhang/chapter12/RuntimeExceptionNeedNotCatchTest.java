f898f759d13ea80da5ec4cfcef365c5d4d76e2d4
mcxtzhang <mcxtzhang@163.com>
2017-03-28 10:58:13 +0800

    【Chap 12】RuntimeException是一个例外，这种异常，不需要catch or throws

---
+++ src/com/mcxtzhang/chapter12/RuntimeExceptionNeedNotCatchTest.java
@@ -0,0 +1,24 @@
+package com.mcxtzhang.chapter12;
+
+/**
+ * Intro: RuntimeException是一个例外，这种异常，不需要catch or throws
+ * Author: zhangxutong
+ * E-mail: mcxtzhang@163.com
+ * Home Page: http://blog.csdn.net/zxt0601
+ * Created:   2017/3/28.
+ * History:
+ */
+public class RuntimeExceptionNeedNotCatchTest {
+    public static void main(String[] args) throws CstException {
+        //throw new RuntimeException("RuntimeException是一个例外，这种异常，不需要catch");
+        throw new CstException("别的异常需要catch  or throws");
+    }
+
+    static class CstException extends Exception {
+        public CstException(String s) {
+            super(s);
+        }
+    }
+
+
+}
