00a0413ff6cc153f224949aa8e33c5bc485380ba
mcxtzhang <mcxtzhang@163.com>
2017-04-06 15:27:52 +0800

    【Chap 16】        //第一种 new，指定length                  A[] a = new A[5];                  //第二种  {}指定具体内容                  A[] b = {new A("a"), new A("ab"), new A("bc"), new A("d")};                  //第三种 new {}指定具体内容                  A[] c = new A[]{new A("a"), new A("b"), new A("c"), new A("d")};

---
+++ src/com/mcxtzhang/chapter15/中文.java
@@ -0,0 +1,21 @@
+package com.mcxtzhang.chapter15;
+
+/**
+ * Intro:
+ * Author: zhangxutong
+ * E-mail: mcxtzhang@163.com
+ * Home Page: http://blog.csdn.net/zxt0601
+ * Created:   2017/4/6.
+ * History:
+ */
+public class 中文 {
+    public static void main(String[] args)
+
+    {
+        中文方法();
+    }
+
+    public static void 中文方法() {
+        System.out.println("我的天!");
+    }
+}
---
+++ src/com/mcxtzhang/chapter16/CreateArrayTest.java
@@ -0,0 +1,33 @@
+package com.mcxtzhang.chapter16;
+
+/**
+ * Intro: 初始化数组的各种方式
+ * Author: zhangxutong
+ * E-mail: mcxtzhang@163.com
+ * Home Page: http://blog.csdn.net/zxt0601
+ * Created:   2017/4/6.
+ * History:
+ */
+public class CreateArrayTest {
+    public static void main(String[] args) {
+        //第一种 new，指定length
+        A[] a = new A[5];
+        //第二种  {}指定具体内容
+        A[] b = {new A("a"), new A("ab"), new A("bc"), new A("d")};
+        //第三种 new {}指定具体内容
+        A[] c = new A[]{new A("a"), new A("b"), new A("c"), new A("d")};
+
+
+    }
+
+    private static class A {
+        String name;
+
+        public A() {
+        }
+
+        public A(String name) {
+            this.name = name;
+        }
+    }
+}
