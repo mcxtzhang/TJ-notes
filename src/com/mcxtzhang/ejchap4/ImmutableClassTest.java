2adf46f8b535618972ce18db8fa0361fb4ec7278
mcxtzhang <mcxtzhang@163.com>
2017-04-11 16:17:20 +0800

    【EJ Chap 4】不可变的类的测试，修改这些类的值，是改变引用 。

---
+++ src/com/mcxtzhang/ejchap4/ImmutableClassTest.java
@@ -0,0 +1,26 @@
+package com.mcxtzhang.ejchap4;
+
+/**
+ * Intro: 不可变的类的测试，修改这些类的值，是改变引用 。
+ * Author: zhangxutong
+ * E-mail: mcxtzhang@163.com
+ * Home Page: http://blog.csdn.net/zxt0601
+ * Created:   2017/4/11.
+ * History:
+ */
+public class ImmutableClassTest {
+    public static void main(String[] args) {
+        String s = new String("a");
+        System.out.println(s.hashCode());
+        s = "b";
+        System.out.println(s.hashCode());
+
+        Integer integer = new Integer(1);
+        System.out.println(integer.hashCode());
+        integer = 2;
+        System.out.println(integer.hashCode());
+
+
+
+    }
+}
