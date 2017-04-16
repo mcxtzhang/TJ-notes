48293db257ae92bd3fe4fe02027b1434133d9821
mcxtzhang <mcxtzhang@163.com>
2017-04-16 16:52:41 +0800

    【EJ Chap  6】 32  用EnumSet代替位域

---
+++ src/com/mcxtzhang/cjchap6/Test32.java
@@ -0,0 +1,22 @@
+package com.mcxtzhang.cjchap6;
+
+import java.util.EnumSet;
+
+/**
+ * Intro: 用EnumSet代替位域
+ * Author: zhangxutong
+ * E-mail: mcxtzhang@163.com
+ * Home Page: http://blog.csdn.net/zxt0601
+ * Created:   2017/4/16.
+ * History:
+ */
+public class Test32 {
+    public static void main(String[] args) {
+        EnumSet<Style> bold = EnumSet.of(Style.BOLD, Style.ITALIC);
+        System.out.println(bold);
+    }
+
+    enum Style {
+        BOLD, ITALIC, UNDERLINE;
+    }
+}
