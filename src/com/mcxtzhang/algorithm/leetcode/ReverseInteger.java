32e3dc8ccdb8e5d10122e9b09003ecddfc343a52
mcxtzhang <mcxtzhang@163.com>
2017-07-31 22:03:24 +0800

    【Java】Reverse digits of an integer.        * <p>        * Example1: x = 123, return 321        * Example2: x = -123, return -321

---
+++ src/com/mcxtzhang/algorithm/leetcode/ReverseInteger.java
@@ -0,0 +1,31 @@
+package com.mcxtzhang.algorithm.leetcode;
+
+/**
+ * +
+ * Reverse digits of an integer.
+ * <p>
+ * Example1: x = 123, return 321
+ * Example2: x = -123, return -321
+ * Created by pc on 2017/7/31.
+ */
+public class ReverseInteger {
+    public static void main(String[] srgs){
+        System.out.println(new Solution().reverse(123));
+        System.out.println(new Solution().reverse(-123));
+    }
+
+
+    public static class Solution {
+        public int reverse(int x) {
+            if (x == 0) return 0;
+
+            StringBuilder sb = new StringBuilder();
+            if (x < 0) {sb.append("-"); x = -x;}
+            while (x != 0) {
+                sb.append(x % 10);
+                x = x / 10;
+            }
+            return Integer.parseInt(sb.toString());
+        }
+    }
+}
