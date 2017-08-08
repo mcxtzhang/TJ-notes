7fc9a498c5e65694ace93c64ebd327291fd86ecb
mcxtzhang <mcxtzhang@163.com>
2017-08-08 14:40:00 +0800

    【Java】硬币找零

--- src/com/mcxtzhang/algorithm/traditional/GetCoinsCount.java
+++ src/com/mcxtzhang/algorithm/traditional/GetCoinsCount.java
@@ -1,5 +1,7 @@
 package com.mcxtzhang.algorithm.traditional;
 
+import java.util.Arrays;
+
 /**
  * Intro: 硬币找零
  * Author: zhangxutong
@@ -11,15 +13,43 @@ package com.mcxtzhang.algorithm.traditional;
 public class GetCoinsCount {
     public static void main(String[] args) {
         // 硬币面值预先已经按降序排列
-        int[] coinValue = new int[] { 25, 21, 10, 5, 1 };
+        int[] coinValue = new int[]{25, 21, 10, 5, 1};
         // 需要找零的面值
         int money = 63;
         // 保存每一个面值找零所需的最小硬币数，0号单元舍弃不用，所以要多加1
         int[] coinsUsed = new int[money + 1];
+        Arrays.fill(coinsUsed, -1);
 
-        getCoinsCount(coinValue,63,coinsUsed);
+        //getCoinsCount(coinValue,63,coinsUsed);
+        System.out.println(getTargetCoinsCount(coinValue, 63, coinsUsed));
         //System.out.println(coinsUsed[63]);
     }
+
+    /**
+     * @param moneys 货币种类面值数组
+     * @param target 需要找零的钱数
+     * @param result 下标为需要找零的钱值， 值为 钱的数量
+     */
+    public static int getTargetCoinsCount(int[] moneys, int target, int[] result) {
+        if (moneys == null || moneys.length < 1 || target < 1 || result == null)
+            return 0;
+        if (result[target] != -1) {
+            return result[target];
+        }
+
+        int temp = 0;
+        for (int money : moneys) {//循环每一种面值
+            if (money <= target) {
+                int targetCoinsCount = getTargetCoinsCount(moneys, target - money, result);
+                if (temp == 0 || temp > targetCoinsCount + 1) {
+                    temp = targetCoinsCount + 1;
+                }
+            }
+        }
+        result[target] = temp;
+        return temp;
+    }
+
 
     /**
      * @param moneys 货币种类面值数组
@@ -42,8 +72,5 @@ public class GetCoinsCount {
             System.out.println("面值为 " + (i) + " 的最小硬币数 : "
                     + result[i]);
         }
-
-
-
     }
 }
