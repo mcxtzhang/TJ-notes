64a2332391925efd4872654a7aec0aa71d327e6f
mcxtzhang <mcxtzhang@163.com>
2017-08-15 22:32:58 +0800

    【Java】 Test35_SearchInsertPosition

---
+++ src/com/mcxtzhang/algorithm/leetcode/Test35_SearchInsertPosition.java
@@ -0,0 +1,33 @@
+package com.mcxtzhang.algorithm.leetcode;
+
+/**
+ * Intro:
+ * Author: zhangxutong
+ * E-mail: mcxtzhang@163.com
+ * Home Page: http://blog.csdn.net/zxt0601
+ * Created:   2017/8/15.
+ * History:
+ */
+public class Test35_SearchInsertPosition {
+    public class Solution {
+        public int searchInsert(int[] nums, int target) {
+            if(null==nums || nums.length<1) return 0;
+
+            int l = 0;
+            int r = nums.length-1;
+            int m ;
+            while(l<=r){
+                m = (l+r)>>>1;
+                if(nums[m]==target){
+                    return m;
+                }else if(nums[m]>target){
+                    r=m-1;
+                }else{
+                    l=m+1;
+                }
+            }
+            return l;
+
+        }
+    }
+}
