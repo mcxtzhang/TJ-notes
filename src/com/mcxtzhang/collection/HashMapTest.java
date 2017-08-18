43b7932f3461d03bd988f7f088542cd0f80c615c
zhangxutong <zhangxutong@imcoming.cn>
2017-08-18 18:07:47 +0800

    【Java】HashMap

---
+++ src/com/mcxtzhang/collection/HashMapTest.java
@@ -0,0 +1,20 @@
+package com.mcxtzhang.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Intro:
+ * Author: zhangxutong
+ * E-mail: mcxtzhang@163.com
+ * Home Page: http://blog.csdn.net/zxt0601
+ * Created:   2017/8/18.
+ * History:
+ */
+public class HashMapTest {
+    public static void main(String[] args){
+        Map<String,String> map = new HashMap<>();
+        map.put("2","5");
+        System.out.println(map);
+    }
+}
