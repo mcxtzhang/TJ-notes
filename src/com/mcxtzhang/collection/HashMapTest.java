36c7bd98d54a01091a75f42284a17f6d39584114
mcxtzhang@163.com <sunwentao@imcoming.cn>
2017-08-18 23:37:27 +0800

    【Java】Test442

---
+++ src/com/mcxtzhang/collection/HashMapTest.java
@@ -0,0 +1,26 @@
+package com.mcxtzhang.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Intro:
+ * Author: zhangxutong
+ * E-mail: mcxtzhang@163.com
+ * Home Page: http://blog.csdn.net/zxt0601
+ * Created:   2017/8/18.
+ * History:
+ */
+public class HashMapTest {
+    public static void main(String[] args){
+    Map<String,String> map = new HashMap<>();
+    map.put("1","1");
+    map.put(null,null);
+    map.put("2",null);
+    map.put("13","13");
+    map.put(null,"f");
+
+
+        System.out.println(map);
+    }
+}
