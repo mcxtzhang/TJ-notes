cb1a3a0901d31afe0cbf579ea08064895b23d56c
mcxtzhang <mcxtzhang@163.com>
2017-09-04 10:20:39 +0800

    【Java】T86. Partition List

--- src/com/mcxtzhang/algorithm/leetcode/ListNode/Test86_Partition_List.java
+++ src/com/mcxtzhang/algorithm/leetcode/ListNode/Test86_Partition_List.java
@@ -32,10 +32,36 @@ public class Test86_Partition_List {
         node5.next = node6;
 
 
-        ListNode partition = new Solution().partition(head, 3);
+        ListNode partition = new Solution2().partition(head, 3);
         System.out.println(partition);
 
 
+    }
+
+
+    static class Solution2 {
+        public ListNode partition(ListNode head, int x) {
+            ListNode dummy1 = new ListNode(0);
+            ListNode dummy2  = new ListNode(0);
+            ListNode cur1 = dummy1,cur2 = dummy2;
+
+            while(head!=null){
+                if(head.val<x){
+                    cur1.next = head;
+                    cur1= cur1.next;
+                }else{
+                    cur2.next = head;
+                    cur2 = cur2.next;
+                }
+
+                head = head.next;
+            }
+            cur2.next = null;
+            cur1.next = dummy2.next;
+
+            return dummy1.next;
+
+        }
     }
 
     /**
