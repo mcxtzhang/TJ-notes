65a48b4715876fb8bca43d46c565387a5af377cb
mcxtzhang <mcxtzhang@163.com>
2017-11-01 10:27:01 +0800

    【Java】快速排序 和 二分查找

--- src/com/mcxtzhang/impotant/QuickSortTst.java
+++ src/com/mcxtzhang/impotant/QuickSortTst.java
@@ -9,9 +9,10 @@ package com.mcxtzhang.impotant;
  * History:
  */
 public class QuickSortTst {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] src = new int[]{8, 3, 1, 4, 5, 9, 2};
-        quickSort(src, 0, src.length - 1);
+        //quickSort(src, 0, src.length - 1);
+        quick20171101(src, 0, src.length - 1);
         for (int i : src) {
             System.out.println(i);
         }
@@ -44,4 +45,38 @@ public class QuickSortTst {
         quickSort(nums, l + 1, end);
 
     }
+
+    //2017 11 01 快速排序
+    public static void quick20171101(int[] nums, int begin, int end) {
+        if (nums == null || nums.length < 1 || begin < 0 || end < 0 || begin > nums.length-1 || end > nums.length-1 || begin >= end)
+            return;
+        int temp = nums[begin];
+        int l = begin;
+        int r = end;
+        while (l < r) {
+            while (l < r && nums[r] >= temp) {
+                r--;
+            }
+            if (l < r) {
+                nums[l] = nums[r];
+                l++;
+            }
+            while (l < r && nums[l] <= temp) {
+                l++;
+            }
+            if (l < r) {
+                nums[r] = nums[l];
+                r--;
+            }
+
+
+        }
+        nums[l] = temp;
+        quick20171101(nums, begin, l - 1);
+        quick20171101(nums, r + 1, end);
+
+
+    }
+
+
 }
